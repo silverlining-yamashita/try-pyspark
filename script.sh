de76c77f8d4a34d42fab2dae7cc8eb2dfc530698
Kenichiro Yamashita <silverlining.yamashita@gmail.com>
2023-11-10 17:43:44 +0900

    scriptを用意し、requirementsでpipをインストールに変更。かつ、需要予測系のライブラリをインストールした。

---
+++ script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+pip3 install -r requirements1.txt --break-system-packages --no-cache-dir
+pip3 install -r requirements2.txt --break-system-packages --no-cache-dir
+pip3 install -r requirements3.txt --break-system-packages --no-cache-dir
